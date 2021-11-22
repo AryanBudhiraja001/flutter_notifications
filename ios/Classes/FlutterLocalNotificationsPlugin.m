f30a6dc6bbf451fe203400f7b5dff837cc1cc9fa
AryanBudhiraja001 <77393754+AryanBudhiraja001@users.noreply.github.com>
2021-11-22 16:45:30 +0530

    Update FlutterLocalNotificationsPlugin.m
    commit

--- ios/Classes/FlutterLocalNotificationsPlugin.m
+++ ios/Classes/FlutterLocalNotificationsPlugin.m
@@ -421,7 +421,7 @@ static FlutterError *getFlutterError(NSError *error) {
         NSTimeInterval timeInterval = 0;
         switch([arguments[REPEAT_INTERVAL] integerValue]) {
             case EveryMinute:
-                timeInterval = 60 * 5;
+                timeInterval = 60 * 2;
                 notification.repeatInterval = NSCalendarUnitMinute;
                 break;
             case Hourly:
@@ -640,7 +640,7 @@ static FlutterError *getFlutterError(NSError *error) {
 - (UNTimeIntervalNotificationTrigger *)buildUserNotificationTimeIntervalTrigger:(id)arguments  API_AVAILABLE(ios(10.0)){
     switch([arguments[REPEAT_INTERVAL] integerValue]) {
         case EveryMinute:
-            return [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:60 * 5
+            return [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:60 * 2
                                                                       repeats:YES];
         case Hourly:
             return [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:60 * 60
